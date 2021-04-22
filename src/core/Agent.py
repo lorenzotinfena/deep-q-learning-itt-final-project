bff216c99967b42660a56d31a49cf117082659f9
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2021-04-23 00:26:39 +0200

    Update Agent.py

--- src/core/Agent.py
+++ src/core/Agent.py
@@ -16,10 +16,13 @@ class Agent:
 				- render(mode='human')
 		"""
 		self.env = env
-	def start_episode(self, discount_factor: float, learning_rate: float):
+	def start_episode(self, discount_factor: float, learning_rate: float, exploration_factor: float = 0, monitor=False):
 		""" start the episode, finish when enviroment return done=True
 		Args:
 			discount_factor: how much the immediate rewards matter than future rewards
 				0 <= discount_factor <= 1
 			learning_rate: with how much strength you want to learn
+				0 < learning_rate
+			exploration_factor: 
 		"""
+	
