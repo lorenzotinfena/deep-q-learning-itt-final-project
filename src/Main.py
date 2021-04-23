f80628744e24c503f1427ab253014d20e4edaf40
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2021-04-23 23:30:11 +0200

    sfsdf

--- src/Main.py
+++ src/Main.py
@@ -1,10 +1,40 @@
-from core.Agent import Agent
+from core.DQNAgent import DQNAgent
 import gym
 import numpy as np
 import torch
 
+import pyvirtualdisplay
+_display = pyvirtualdisplay.Display(visible=False, size=(1400, 900))
+_ = _display.start()
 
 env = gym.make("CartPole-v1")
+env = gym.wrappers.Monitor(env, 'recording', force=True)
 print('state_space / observation_space: ' + str(np.array(env.observation_space)))
 print('action_space: ' + str(env.action_space))
-agent = Agent(env)
+agent = DQNAgent(env)
+
+DISCOUNT_FACTOR = 0.9
+LEARNING_RATE = 0.6
+total_rewards = []
+#evaluating
+for i in range(10):
+    total_reward, steps = agent.start_episode(DISCOUNT_FACTOR, LEARNING_RATE, 0, True)
+    print(total_reward)
+    total_rewards.append(total_reward)
+print(total_rewards)
+print('mean: ' + str(np.array(total_rewards).mean()))
+#training
+for i in range(20):
+    total_reward, steps = agent.start_episode(DISCOUNT_FACTOR, LEARNING_RATE, 1, True)
+    print(total_reward)
+    total_rewards.append(total_reward)
+print(total_rewards)
+print('mean: ' + str(np.array(total_rewards).mean()))
+total_rewards = []
+#evaluating
+for i in range(10):
+    total_reward, steps = agent.start_episode(DISCOUNT_FACTOR, LEARNING_RATE, 0, True)
+    print(total_reward)
+    total_rewards.append(total_reward)
+print(total_rewards)
+print('mean: ' + str(np.array(total_rewards).mean()))
--- src/core/DQNAgent.py
+++ src/core/DQNAgent.py
@@ -1,8 +1,9 @@
 
 import gym
 from gym import wrappers
+from core.Net import Net
+import random
 import numpy as np
-
 
 class DQNAgent:
 	""" Deep Q learning agent with
@@ -19,6 +20,7 @@ class DQNAgent:
 				and action_space is discrete:------------------------
 		"""
 		self.env = env
+		self.nn = Net(env.observation_space.shape[0], env.action_space.n, 0.5)
 	def start_episode(self, discount_factor: float, learning_rate: float, exploration_epsilon: float = 0, monitor=False):
 		""" start the episode, finish when enviroment return done=True
 			Use epsilon-greedy algorithm to 
@@ -38,16 +40,28 @@ class DQNAgent:
 
 		# get the first state
 		current_state = self.env.reset()
-
+		current_state = current_state.reshape(1, -1)
 		while not done:
 			# choose action
-			action = self.env.action_space.sample() if random.uniform(0, 1) < exploration_probability else np.argmax(self.nn.forward(current_state))
+			q_values = self.nn.model.predict(current_state)
+			action = self.env.action_space.sample()  if random.uniform(0, 1) < exploration_epsilon else np.argmax(q_values)
 
 			next_state, reward, done, _ = self.env.step(action)
+			next_state = next_state.reshape(1, -1)
 			if monitor:
 				total_reward += reward
 				steps += 1
-			#update the
+				self.env.render()
+			
+			# find target q(s)
+			q_values_target = np.copy(q_values)
+			q_values_target[0][action] = float = reward + discount_factor * np.max(self.nn.model.predict(next_state))
+
+			# update neural network
+			self.nn.model.fit(current_state, q_values_target, epochs=1, verbose=0)
+
+			# set current state
+			current_state = next_state
 
 
 		if monitor: return total_reward, steps
